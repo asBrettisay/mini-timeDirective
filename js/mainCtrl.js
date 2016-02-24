f1431320d20844eefac264179ea523e69ebd78ef
asBrettisay <brett.caudill.is@gmail.com>
2016-02-23 20:13:12 -0700

    finished.

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,4 +1,13 @@
 angular.module('timeApp')
 .controller('mainCtrl', function($scope) {
   $scope.name = 'Brett'
+  $scope.format = "medium";
+
+  $scope.updateFormat = function() {
+    if ($scope.format === 'medium') {
+      $scope.format = 'short'
+    } else {
+      $scope.format = 'medium'
+    }
+  }
 });
--- js/timeDirective.js
+++ js/timeDirective.js
@@ -2,10 +2,28 @@ angular.module('timeApp')
 .directive('showTime', function() {
   return {
     restrict: 'E',
-    template: '<div>The current time is {{time}}</div>',
+    scope: {
+      format: '=',
+      update: '&',
+      newColor: '@'
+    },
+    template: '<strong><div>The current time is {{time | date: format}}</div><div>GMT time is {{gmtTime | date: format }}</div></strong>',
     link: function(scope, element, attrs) {
       var currentTime = new Date();
-      scope.time = currentTime.toString();
+
+      element.css({
+        color: scope.newColor
+      })
+
+      element.on('mousedown', function() {
+        scope.time = new Date();
+        scope.gmtTime = new Date(scope.time.toGMTString());
+        scope.update();
+        scope.$apply();
+      })
+
+      scope.time = currentTime;
+      scope.gmtTime = new Date(currentTime.toGMTString());
     }
   }
 })
